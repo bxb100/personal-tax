e889ccfc2aed7d96fc68dfc052d4366e90560ae7
John <bxb100@outlook.com>
2021-11-18 20:02:45 +0800

    Initial commit

---
+++ src/main/java/com/pers/personaltax/PersonalTaxApplication.java
@@ -0,0 +1,13 @@
+package com.pers.personaltax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalTaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pers/personaltax/PersonalTaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pers.personaltax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
